e511c1987c0bf8d27905c6daaa72fd012d9f2e02
Chris B <cbosoft@protonmail.com>
2022-08-03 20:51:29 +0100

    Add test for one run through of trainer

---
+++ tests/train_test.py
@@ -0,0 +1,14 @@
+from mldb.config import CONFIG
+
+from mask_rcnn.config import get_config, finalise
+from mask_rcnn.actions.trainer import Trainer
+
+
+def test_trainer():
+    cfg = get_config()
+    cfg.data.pattern = '../data/MISC_DEV_TEST_DATA.json'
+    cfg.model.n_classes = 4
+    cfg.training.n_epochs = 1
+    finalise(cfg)
+    trainer = Trainer(cfg)
+    trainer.train()
