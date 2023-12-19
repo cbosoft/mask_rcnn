85197cfbd8150b6a5d77eb1290274ad08119d2e3
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2023-12-19 14:56:04 +0000

    Add option to override config output dir
    e.g. when continuing a previous job

--- mask_rcnn/run.py
+++ mask_rcnn/run.py
@@ -3,22 +3,31 @@ from .config import CfgNode, get_config, finalise
 from .actions import build_action
 
 
-def run_experiment(config_file_path: str):
+def run_experiment(config_file_path: str, **kwargs):
     cfg = get_config()
     cfg.merge_from_file(config_file_path)
-    run_config(cfg)
+    run_config(cfg, **kwargs)
 
 
-def run_config(cfg: CfgNode):
+def run_config(cfg: CfgNode, specified_output_dir: str = None):
+    if specified_output_dir is not None:
+        cfg.output_dir = specified_output_dir
     finalise(cfg)
 
     with build_action(cfg) as action:
         action.act()
 
 
-def run_template(template_filename: str):
+def run_template(template_filename: str, specified_output_dir: str = None):
     cfg = get_config()
     variants = cfg.apply_template(template_filename)
     print(f'Running templated experiment "{template_filename}" ({len(variants)} permutations)')
-    for variant in variants:
-        run_config(variant)
+    for i, variant in enumerate(variants):
+        run_config(variant, specified_output_dir=f'{specified_output_dir}/{i}')
+
+
+def run(filename: str, specified_output_dir: str = None):
+    if filename.endswith('syt'):
+        run_template(filename, specified_output_dir=specified_output_dir)
+    else:
+        run_experiment(filename, specified_output_dir=specified_output_dir)
